5dc9e3f42159794d3a7a63ebf73f7f24a15c22b7
ramgmtk <ramgmtk@gmail.com>
2020-05-29 01:55:06 -0700

    Main menu, game over menu options

--- src/scenes/gameOver.js
+++ src/scenes/gameOver.js
@@ -6,26 +6,18 @@ class GameOver extends Phaser.Scene {
         this.data = data;
     }
     create() {
-        this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-        let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
-            align: 'right',
-            padding: {
-                top: 5,
-                bottom: 5,
-            },
-            fixedWidth: 0
-        }
-        this.add.text(centerX , centerY - 32, 'GAME OVER' , menuConfig).setOrigin(.5);
-        this.add.text(centerX, centerY, 'PRESS SPACE TO RESTART', menuConfig).setOrigin(0.5);
-    }
+        let gameover = this.add.image(centerX, centerY, 'gameoverButton', null).setOrigin(0.5).setDepth(uiDepth);
+        let restartButton = this.add.image(centerX, centerY + 150, 'restartButton', null).setOrigin(0.5).setDepth(uiDepth);
 
-    update() {
-        if (Phaser.Input.Keyboard.JustDown(this.space)) {
-            this.scene.start('menuScene')
-        } 
+        restartButton.setInteractive();
+        restartButton.on('pointerover', () => {
+            restartButton.setScale(1.5);
+        });
+        restartButton.on('pointerout', () => {
+            restartButton.setScale(1.0);
+        });
+        restartButton.on('pointerdown', () => {
+            this.scene.start('menuScene');
+        });
     }
 }
--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -4,9 +4,9 @@ class Menu extends Phaser.Scene {
     }
     preload() {
         let loading = this.add.graphics();
-        loading.fillStyle(0xffffff, 1);
-        loading.fillRect(game.config.width * 0.1, centerY - 20, game.config.width * 0.8, 40)
         this.load.on('progress', (value) => {
+            loading.fillStyle(0xffffff, 1);
+            loading.fillRect(game.config.width * 0.1, centerY - 20, game.config.width * 0.8, 40)
             loading.fillStyle(0x00ff00, 1);
             loading.fillRect(game.config.width * 0.1, centerY - 20, game.config.width * 0.8 * value, 40);
         });
@@ -24,7 +24,18 @@ class Menu extends Phaser.Scene {
         this.load.atlas('keyAtlas', 'keys.png', 'keys.json');
         this.load.path = './assets/tilemap/';
         this.load.image('tutorial_tile', 'Tile_Sheet_v2.png');
-        this.load.tilemapTiledJSON('tutorial_map', 'Tutorial1.json')
+        this.load.tilemapTiledJSON('tutorial_map', 'Tutorial1.json');
+
+        this.load.path = './assets/Menu/';
+        this.load.image('backButton', 'back.png');
+        this.load.image('controlsButton', 'controls.png');
+        this.load.image('gameoverButton', 'gameover.png');
+        this.load.image('playButton', 'play.png');
+        this.load.image('restartButton', 'restart.png');
+        this.load.image('upButton', 'up.png');
+        this.load.image('downButton', 'down.png');
+        this.load.image('volumeButton', 'volume.png');
+
         this.load.path = './assets/';
         this.load.image('bg', 'bg.png');
         this.load.image('shield', 'shield.png');
@@ -48,8 +59,7 @@ class Menu extends Phaser.Scene {
 
     create() {
         this.createAnimations();
-        this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-        let menuConfig = {
+        /*let menuConfig = {
             fontFamily: 'Courier',
             fontSize: '28px',
             backgroundColor: '#F3B141',
@@ -63,15 +73,77 @@ class Menu extends Phaser.Scene {
         }
         this.add.text(centerX , centerY - 32, 'Final Game Prototype' , menuConfig).setOrigin(.5);
         this.add.text(centerX, centerY, 'WASD to move, F melee attack, hijkl note keys', menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY + 32, 'Press SPACE to play song / start', menuConfig).setOrigin(0.5);
-    }
+        this.add.text(centerX, centerY + 32, 'Press SPACE to play song / start', menuConfig).setOrigin(0.5);*/
 
-    update() {
-        if (Phaser.Input.Keyboard.JustDown(this.space)) {
+        let volumeLevel = audio * 10;
+        let playButton = this.add.image(centerX, centerY, 'playButton', null).setOrigin(0.5).setDepth(uiDepth);
+        let controlsButton = this.add.image(centerX, centerY + 150, 'controlsButton', null).setOrigin(0.5).setDepth(uiDepth);
+        let volumeControl = this.add.image(centerX - 64, centerY + 350, 'volumeButton', null).setOrigin(0.5).setDepth(uiDepth);
+        let upButton = this.add.image(centerX + 218/2, centerY + 286, 'upButton', null).setOrigin(0.5).setDepth(uiDepth);
+        let downButton = this.add.image(centerX + 218/2, centerY + 414, 'downButton', null).setOrigin(0.5).setDepth(uiDepth);
+
+        let volume = this.add.text(centerX + 218/2, centerY + 350, `${volumeLevel}`, {
+            fontFamily: 'Courier',
+            fontSize: '64px',
+            color: '#ffffff',
+
+        }).setOrigin(0.5).setDepth(uiDepth);
+
+        playButton.setInteractive();
+        playButton.on('pointerover', () => {
+            playButton.setScale(1.5);
+        });
+        playButton.on('pointerout', () => {
+            playButton.setScale(1.0);
+        });
+        playButton.on('pointerdown', () => {
             this.scene.add('gameScene', Game);
             this.scene.add('bossScene', BossRoom);
             this.scene.start('gameScene');
-        }
+        });
+
+        controlsButton.setInteractive();
+        controlsButton.on('pointerover', () => {
+            controlsButton.setScale(1.5);
+        });
+        controlsButton.on('pointerout', () => {
+            controlsButton.setScale(1.0);
+        });
+
+        upButton.setInteractive();
+        upButton.on('pointerover', () => {
+            upButton.setScale(1.5);
+        });
+        upButton.on('pointerout', () => {
+            upButton.setScale(1.0);
+        });
+
+        //Credit to user m93a for explanation of math.round
+        //https://stackoverflow.com/questions/2283566/how-can-i-round-a-number-in-javascript-tofixed-returns-a-string
+        upButton.on('pointerdown', () => {
+            if (audio < 1.0) {
+                audio += 0.1;
+                audio = Math.round(audio * 1e1) / 1e1;
+                volumeLevel = audio * 10;
+                volume.setText(`${volumeLevel}`);
+            }
+        });
+
+        downButton.setInteractive();
+        downButton.on('pointerover', () => {
+            downButton.setScale(1.5);
+        });
+        downButton.on('pointerout', () => {
+            downButton.setScale(1.0);
+        });
+        downButton.on('pointerdown', () => {
+            if (audio > 0.0) {
+                audio -= 0.1;
+                audio = Math.round(audio * 1e1) / 1e1;
+                volumeLevel = audio * 10;  
+                volume.setText(`${volumeLevel}`);
+            }
+        });
     }
 
     createAnimations() {
